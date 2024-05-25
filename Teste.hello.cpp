3482f17dd2ce93cc4880bbd50630cd711be30e30
Thaissa Santana <thaissasantana.dev@gmail.com>
2024-05-25 17:32:10 -0300

    Commit do hello em C

---
+++ Teste.hello.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main ()
+{	
+	printf("Hello World!");
+	return 0;
+	
+}
+
+// 4 bytes
+int idadeDaVanessa = 68;
+
+// 1 byte
+char arroba = "@";
+
+// 4 bytes (6 casas decimais)
+float saldoDaConta = 18923.12;
+
+// 8 bytes (15 casas decimais)
+double chancesGabrielSemBriga = 0.00001;
+
+// 4 bytes * 7 posições = 28 bytes
+int diasDormirNoSofa[7] = {19,20,21,22,23,24,25};
+
+// 3 bytes * 10 posições = 30 bytes
+char tarefasGabriel[3][10] = {
+	"Varrer",
+	"Louça",
+	"Jantar"
+};
+
+
