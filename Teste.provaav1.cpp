3e4d0121aa97f4e46e9abd4cb297f6cf804f88ee
Thaissa Santana <thaissasantana.dev@gmail.com>
2024-05-25 17:38:50 -0300

    Commit testando exercícios

---
+++ Teste.provaav1.cpp
@@ -0,0 +1,59 @@
+// #include <stdio.h>
+
+    //main(){
+
+  //             float saldoAtual, valor;
+//               char tipo;
+            //	 printf("Informe o saldo atual da sua conta:");
+            //   scanf("%f", &saldoAtual);
+            //   printf("Informe o valor:");
+          //     scanf("%f", &valor);
+        //       saldoAtual =  saldoAtual - valor;
+      //         printf("saldo agora = %f \n",saldoAtual);
+    //           if (saldoAtual<0)
+  //                printf("conta estourada!!!\n");
+
+//}
+
+//#include <stdio.h>
+
+//main(){
+//               float nota1, nota2, media;
+//
+//               printf("informe a primeira nota:");
+//               scanf("%f",&nota1);
+//
+//               printf("Informe a segunda nota:");
+//               scanf("%f",&nota2);
+//
+//               media = nota1+nota2/2;
+//               printf("media = %f \n",media);
+//
+//               if(media >=7)
+//                  printf("aprovado");
+//}
+
+#include <stdio.h>
+
+main(){
+               float temp1, temp2;
+
+               printf("informe a temperatura em Salvador:");
+               scanf("%f",&temp1);
+
+               printf("Informe a temperatura em São Paulo:");
+               scanf("%f",&temp2);
+
+               printf("%f - %f",temp1, temp2);
+
+               if(temp1 <= temp2)
+                  printf("Inédito");
+}
+
+
+
+
+
+
+
+
